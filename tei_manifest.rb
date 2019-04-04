bfb58ac365683bc5fb8954f455da8d1656aa92ef
Doug Emery <emeryr@upenn.edu>
2019-04-04 15:45:41 -0400

    Improve logging output

--- tei_manifest.rb
+++ tei_manifest.rb
@@ -33,26 +33,37 @@ def get_checksum file, blocksize: DEFAULT_BLOCK_SIZE
   digest.hexdigest
 end # def get_checksum file, blocksize: DEFAULT_BLOCK_SIZE
 
+LOGGER.info(CMD) { "Generating Walters TEI manifest" }
+
 Dir.chdir TEI_DIRECTORY do
   changed = false
   data = {}
+  files = GLOBS.flat_map { |glob| Dir[glob] }
   if File.exist? MANIFEST
+    LOGGER.info(CMD) { "Manifest exists; checking for new/updated files" }
     manifest_mtime = File.mtime MANIFEST
     data = manifest_data MANIFEST
-    files = GLOBS.flat_map { |glob| Dir[glob] }
     files.each do |f|
       next if data.include?(f) && manifest_mtime > File.mtime(f)
+      if data.include? f
+        LOGGER.info(CMD) { "Setting checksum for updated file: #{f}" }
+      else
+        LOGGER.info(CMD) { "Setting checksum for new file: #{f}" }
+      end
       data[f] = get_checksum f
       changed = true
     end
   else
+    LOGGER.debug(CMD) { "No manifest exists; generating all checksums" }
     data = files.inject({}) { |hash,f| 
+      LOGGER.debug(CMD) { "Setting checksum for file: #{f}" }
       hash[f] = get_checksum f
       hash
     }
     changed = true
   end
   if changed
+    LOGGER.info(CMD) { "New data for manifest found; preparing new manifest" }
     begin
       tmp_file = Tempfile.new
       data.each { |file,checksum| tmp_file.puts "#{checksum}  #{file}" }
@@ -76,7 +87,7 @@ Dir.chdir TEI_DIRECTORY do
       end
     end
   else
-    puts "INFO: No changes made to #{MANIFEST}"
+    LOGGER.info(CMD) { "INFO: No changes made to #{MANIFEST}" }
   end
 end
 
